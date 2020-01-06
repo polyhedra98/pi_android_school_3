8cdada0cce8df907fd37ed3970e6e6ef822381e8
polyhedra98 <1assassin19741@gmail.com>
2020-01-06 17:22:28 +0200

    Finished setting up, preserving, restoring and observing a map. Search button is not yet implemented.

--- app/src/main/java/com/mishenka/notbasic/fragments/MapFragment.kt
+++ app/src/main/java/com/mishenka/notbasic/fragments/MapFragment.kt
@@ -7,21 +7,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import com.mishenka.notbasic.R
 import com.mishenka.notbasic.data.model.FragmentExtras
+import com.mishenka.notbasic.fragments.data.MapFragmentData
 import com.mishenka.notbasic.interfaces.IFragmentRequest
+import com.mishenka.notbasic.managers.preservation.PreservationManager
 import com.mishenka.notbasic.viewmodels.EventVM
+import kotlinx.android.synthetic.main.fragment_map.*
+import org.koin.android.ext.android.get
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private val TAG = "MapFragment"
 
 
     private val eventVM by sharedViewModel<EventVM>()
 
+    private val preservationManager = get<PreservationManager>()
+
+
     private var fragmentId: Long? = null
+
+    private var restoredData: MapFragmentData? = null
+
+    private var map: GoogleMap? = null
+
+    private var lat: Double? = null
+
+    private var lng: Double? = null
 
 
     override fun onCreateView(
@@ -41,7 +64,124 @@ class MapFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        restoredData = (preservationManager.getDataForFragment(fragmentId!!) as? MapFragmentData?)
+
+        lat = restoredData?.lat
+        lng = restoredData?.lng
+
+        if (lat != null && lng != null) {
+            updateLatLng(lat!!, lng!!)
+        }
+
+        setupViews()
     }
+
+
+    override fun onDestroyView() {
+
+        preservationManager.preserveFragmentData(fragmentId!!, MapFragmentData(
+            lat = lat,
+            lng = lng
+        ))
+
+        super.onDestroyView()
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        Log.i("NYA_$TAG", "Map is ready. Is null? ${googleMap == null}}")
+        map = googleMap
+
+        if (lat != null && lng != null) {
+            placeMarker(lat!!, lng!!)
+            centerCamera(lat!!, lng!!)
+        }
+
+        map?.setOnMapLongClickListener {
+            updateLatLng(it.latitude, it.longitude)
+            placeMarker(it.latitude, it.longitude)
+        }
+        map?.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
+
+            override fun onMarkerDragEnd(p0: Marker?) {
+                Log.i("NYA_$TAG", "Marker drag end.")
+                p0?.position?.let { position ->
+                    updateLatLng(position.latitude, position.longitude)
+                }
+            }
+
+            override fun onMarkerDragStart(p0: Marker?) {
+                Log.i("NYA_$TAG", "Marker drag start.")
+            }
+
+            override fun onMarkerDrag(p0: Marker?) {
+                Log.i("NYA_$TAG", "Marker drag.")
+                p0?.position?.let { position ->
+                    updateLatLng(position.latitude, position.longitude)
+                }
+            }
+
+        })
+
+        //TODO("Track location in the future.")
+
+    }
+
+
+    private fun setupViews() {
+        setupMap()
+
+        map_center_b.setOnClickListener {
+            centerCamera(lat!!, lng!!)
+        }
+        map_search_b.setOnClickListener {
+            //TODO("Request map results.")
+        }
+    }
+
+
+    private fun setupMap() {
+        (childFragmentManager.findFragmentById(R.id.map_f) as SupportMapFragment?)?.
+            getMapAsync(this)
+    }
+
+
+    private fun updateLatLng(lat: Double, lng: Double) {
+        this.lat = lat
+        this.lng = lng
+
+        activateCenterButton()
+
+        map_location_tv.text = getString(R.string.location_ui, lat, lng)
+    }
+
+
+    private fun activateCenterButton() {
+        if (!map_center_b.isEnabled) {
+            map_center_b.isEnabled = true
+        }
+    }
+
+
+    private fun placeMarker(lat: Double, lng: Double) {
+        removeAllMarkers()
+        map?.addMarker(MarkerOptions().position(LatLng(lat, lng))
+            .title(getString(R.string.current_location_ui))
+            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+            .draggable(true)
+        )
+    }
+
+
+    private fun removeAllMarkers() {
+        map?.clear()
+    }
+
+
+    private fun centerCamera(lat: Double, lng: Double) {
+        map?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(lat, lng)))
+    }
+
 
 
     object MapRequest : IFragmentRequest {
---
+++ app/src/main/java/com/mishenka/notbasic/fragments/data/MapFragmentData.kt
@@ -0,0 +1,9 @@
+package com.mishenka.notbasic.fragments.data
+
+import com.mishenka.notbasic.interfaces.IFragmentData
+
+
+data class MapFragmentData(
+    val lat: Double?,
+    val lng: Double?
+) : IFragmentData
